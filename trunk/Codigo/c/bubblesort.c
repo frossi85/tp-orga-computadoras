f4b18f02ebcda1d051f5d46bbfa101cbf1fb2ba2
federicomrossi <federicomrossi@ca17bb14-6728-0017-f2ea-4fcdca562c73>
2013-04-20 23:00:37 +0000

    Subida de archivos nombrados en anterior commit

---
+++ trunk/Codigo/c/bubblesort.c
@@ -0,0 +1,22 @@
+/* ****************************************************************************
+ * ****************************************************************************
+ *
+ * LIBRERÍA BUBBLESORT
+ *
+ * ****************************************************************************
+ * ***************************************************************************/
+
+
+#include "bubblesort.h"
+
+
+
+// Función que aplica el algoritmo de ordenamiento Bubblesort para ordenar un
+// arreglo de palabras.
+// PRE: 'words' es un puntero a un arreglo de punteros a caracter; 'arraysize'
+// es el tamaño de dicho arreglo.
+// POST: el arreglo 'words' queda ordenado.
+void bubblesort(char* words[], int arraysize)
+{
+
+}
---
+++ trunk/Codigo/c/main.c
@@ -0,0 +1,44 @@
+/* ****************************************************************************
+ * PROGRAMA DE ORDENAMIENTO DE PALABRAS
+ * ****************************************************************************
+ * 
+ * Facultad de Ingeniería - UBA
+ * 66.20 ORganización de computadoras
+ * Trabajo Práctico N°1: Conjunto de instrucciones MIPS
+ * 
+ * ALUMNOS: 
+ * 
+ * Facundo Rossi () - 
+ * Federico Colangelo () - 
+ * Federico Martín Rossi (92086) - federicomrossi@gmail.com
+ *
+ * ****************************************************************************
+ *
+ * Programa que... [ Colocar texto aquí ]
+ *
+ *
+ *
+ * FORMA DE USO
+ * ============
+ * 
+ *  [ Colocar texto aquí ]
+ *
+ */
+
+
+#include <stdio.h>
+#include "bubblesort.h"
+#include "shellsort.h"
+
+
+
+/* ****************************************************************************
+ * PROGRAMA PRINCIPAL
+ * ***************************************************************************/
+
+int main(int argc, char **argv) {
+	
+
+
+	return 0;
+}
