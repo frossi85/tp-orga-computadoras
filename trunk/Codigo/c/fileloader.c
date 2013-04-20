0742d1b3e84116d0fa65c6e63988ab7dadc7d144
frossi85@gmail.com <frossi85@gmail.com@ca17bb14-6728-0017-f2ea-4fcdca562c73>
2013-04-20 23:52:36 +0000

    - utilidad para cargar archivo de palabras

---
+++ trunk/Codigo/c/fileloader.c
@@ -0,0 +1,20 @@
+/* ****************************************************************************
+ * ****************************************************************************
+ *
+ * LIBRERÍA DE CARGA DE ARCHIVO DE PALABRAS
+ *
+ * ****************************************************************************
+ * ***************************************************************************/
+
+
+#include "fileloader.h"
+
+
+
+// Función que carga desde un archivo de texto las palabras a ordenar.
+// PRE: 'fileUrl' es la url completa al archivo a ordenar.
+// POST: devuelve la lista de palabras cargadas en memoria
+char ** words file_loader(char [] fileUrl);
+{
+
+}
---
+++ trunk/Codigo/c/fileloader.h
@@ -0,0 +1,25 @@
+/* ****************************************************************************
+ * ****************************************************************************
+ *
+ * LIBRERÍA DE CARGA DE ARCHIVO DE PALABRAS
+ *
+ * ****************************************************************************
+ * ***************************************************************************/
+
+
+
+#ifndef FILELOADER_H
+#define FILELOADER_H
+
+
+
+// Función que aplica el algoritmo de ordenamiento Shellsort para ordenar un
+// arreglo de palabras.
+// PRE: 'words' es un puntero a un arreglo de punteros a caracter; 'arraysize'
+// es el tamaño de dicho arreglo.
+// POST: el arreglo 'words' queda ordenado.
+char ** words file_loader(char [] fileUrl);
+
+
+
+#endif
