8ffd5f484738bbde85f624f631dc7aa0af757e99
frossi85 <frossi85@gmail.com>
2013-04-20 23:43:54 +0000

    Biblioteca para comparar dos palabras, usada por los algoritmos de ordenamiento

---
+++ Codigo/c/compare.h
@@ -0,0 +1,8 @@
+// Función que  usada para comparar dos palabras y decidir cual debe ir primero 
+// en una funcion de ordenamiento
+// PRE: 'word1' y 'word2' son las palabras a comparar
+// POST: devuelve un entero indicando el orden
+// - > 0 word1 es mayor a word2
+// - = 0 word1 es igual a word2
+// - < 0 word1 es menor a word2
+int compare(char [] word1, char [] word2);
