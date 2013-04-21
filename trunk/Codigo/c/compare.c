0b5b80d8d11cd536ea0e24f34b088abb2167df90
federicomrossi <federicomrossi@ca17bb14-6728-0017-f2ea-4fcdca562c73>
2013-04-21 17:15:23 +0000

    Comparación de palabras ahora las compara en minúsculas. Se agregó carátula e índice al informe.

--- trunk/Codigo/c/compare.c
+++ trunk/Codigo/c/compare.c
@@ -6,9 +6,43 @@
  * ****************************************************************************
  * ***************************************************************************/
 
-
 #include "compare.h"
+#include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+
+
+/* ****************************************************************************
+ * CONSTANTES
+ * ***************************************************************************/
+
+// Máximo de caracteres permitidos por cadena
+#define MAX_CHARS 100
+
+
+
+
+/* ****************************************************************************
+ * FUNCIONES AUXILIARES
+ * ***************************************************************************/
+
+// Funcion que convierte una palabra a minusculas.
+// PRE: 'palabra' es una cadena de caraceteres.
+// POST: 'palabra' se convirtio en minusculas.
+void palabraAMinuscula(char* palabra)
+{
+	int i;
+	for (i = 0; palabra[i]; i++)
+		palabra[i] = tolower(palabra[i]);
+}
+
+
+
+
+/* ****************************************************************************
+ * FUNCIONES DE LA LIBRERIA
+ * ***************************************************************************/
 
 
 // Funcion que compara dos palabras y determina cual es menor alfanumericamente
@@ -19,5 +53,16 @@
 // - = 0 word1 es igual a word2
 int compare(char* word1, char* word2) 
 {
-	return strcmp(word1, word2);
+	// Creamos una copia de las palabras
+	char w1[MAX_CHARS];
+	char w2[MAX_CHARS];
+	strcpy(w1, word1);
+	strcpy(w2, word2);
+
+	// Convertimos en minusculas las palabras copiadas
+	palabraAMinuscula(w1);
+	palabraAMinuscula(w2);
+
+	// Hacemos comparacion y devolvemos resultado
+	return strcmp(w1, w2);
 }
