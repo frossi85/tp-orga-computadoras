5294061795f72275384345e46796d4a4bad7aba1
federicomrossi@gmail.com <federicomrossi@gmail.com@ca17bb14-6728-0017-f2ea-4fcdca562c73>
2013-05-01 23:08:11 +0000

    Documentación del programa en C terminada

--- trunk/Codigo/c/fileloader.h
+++ trunk/Codigo/c/fileloader.h
@@ -21,10 +21,16 @@
 // POST: devuelve la lista de palabras cargadas en memoria
 char* file_loader(const char * fileUrl);
 
-// [ Colocar documentacion ]
+// Funcion que separa las palabras de un texto en unidades.
+// PRE: 'text' es una cadena que contiene el texto; 'result' es un puntero en donde se
+// insertan las palabras del texto.
+// POST: en 'result' se almacenaron las palabras.
 int to_words(char * text, char *** result);
 
-// [ Colocar documentacion ]
+// Funcion que verifica si un caracter es un fin de palabra o un separador.
+// PRE: 'letter' es el caracter a procesar.
+// POST: devuelve true si el caracter es un fin de palabra o separador, o
+// false en su defecto.
 bool isEndOfWord(char letter);
 
 #endif
